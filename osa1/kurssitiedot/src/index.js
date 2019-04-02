291aca4421ae9f65e62aa4242e16eeb51f383ef1
ninis <nina.m.niskanen@iki.fi>
2019-04-02 20:40:40 +0300

    osa1a valmis

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -9,18 +9,12 @@ const Header = (props) => {
 	)
 }
 
-const Content = (props) => {
+const Part = (props) => {
 	return (
 		<>
 			<p>
-			    {props.part1} {props.exercises1}
-			  </p>
-			  <p>
-			    {props.part2} {props.exercises2}
-			  </p>
-			  <p>
-			    {props.part3} {props.exercises3}
-			  </p>
+			  {props.course} {props.exercises}
+			</p>
 	    </>
 	  )
 }
@@ -45,10 +39,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1 = {exercises1} 
-      			part2={part2} exercises2 = {exercises2}
-      			part3={part3} exercises3 = {exercises3} />
-      
+      <Part course={part1} exercises={exercises1}/>
+      <Part course={part2} exercises={exercises2}/>
+      <Part course={part3} exercises={exercises3}/>
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
