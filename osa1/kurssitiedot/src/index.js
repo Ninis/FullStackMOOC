e760b1fe484f8185dc759efb04e3cec0108751bb
ninis <nina.m.niskanen@iki.fi>
2019-04-03 20:24:51 +0300

    Kotitehtavat 1.3 - 1.5.

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -2,47 +2,51 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = (props) => {
-	return (
-		<>
-			<h1>{props.course}</h1>
-		</>
-	)
+	return <h1>{props.course.name}</h1>
 }
 
 const Part = (props) => {
 	return (
-		<>
-			<p>
-			  {props.course} {props.exercises}
-			</p>
-	    </>
+		  props.course.parts.map((part) => <p>{part.name} {part.exercises}</p>)
 	  )
 }
 
 const Total = (props) => {
+	let total = 0
+
+	props.course.parts.forEach(part => {
+	  total += part.exercises
+	})
+
 	return (
-		<>
-			<p>yhteensä {props.total} tehtävää</p>
-		</>
+			<p>yhteensä {total} tehtävää</p>
 	)
 }
 
 const App = () => {
-  const course = 'Half Stack -sovelluskehitys'
-  const part1 = 'Reactin perusteet'
-  const exercises1 = 10
-  const part2 = 'Tiedonvälitys propseilla'
-  const exercises2 = 7
-  const part3 = 'Komponenttien tila'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Part course={part1} exercises={exercises1}/>
-      <Part course={part2} exercises={exercises2}/>
-      <Part course={part3} exercises={exercises3}/>
-      <Total total={exercises1 + exercises2 + exercises3} />
+	  <Part course={course} />
+      <Total course={course} />
     </div>
   )
 }
